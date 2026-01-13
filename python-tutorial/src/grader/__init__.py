c0c14bbd40960f1611345d4ea58206ce7b51aa1d
Carlos R. Mercado <agvtconsulting@gmail.com>
2026-01-13 11:01:46 -0500

    web only

--- python-tutorial/src/grader/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Grading utilities for Python tutorial exercises."""
-
-from .check import check, check_model, check_plot, check_type, check_error
-
-__all__ = ["check", "check_model", "check_plot", "check_type", "check_error"]
--- python-tutorial/src/webapp/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Web GUI for Python Tutorial."""
